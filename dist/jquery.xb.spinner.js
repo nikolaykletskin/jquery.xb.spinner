588cdff0bf6028677a1007918cee7d92c130427d
Nikolay Kletskin <femalemoustache@gmail.com>
2017-01-24 17:07:33 +0300

    fix change triggering

--- dist/jquery.xb.spinner.js
+++ dist/jquery.xb.spinner.js
@@ -18,16 +18,16 @@
                 content: '+'
             }
         },
-        setDefaults: function(settings) {
+        setDefaults: function (settings) {
             $.extend(true, $.spinner.defaults, settings)
         }
     };
 
-    var downButton = function(obj, settings) {
+    var downButton = function (obj, settings) {
         var button = $(settings.button.element);
         button.addClass(settings.button.className + " " + settings.downButton.className).html(settings.downButton.content);
 
-        button.on('click', function() {
+        button.on('click', function () {
             var value = parseFloat(obj.val());
 
             value -= settings.step;
@@ -44,11 +44,11 @@
         return button;
     };
 
-    var upButton = function(obj, settings) {
+    var upButton = function (obj, settings) {
         var button = $(settings.button.element);
         button.addClass(settings.button.className + " " + settings.upButton.className).html(settings.upButton.content);
 
-        button.on('click', function() {
+        button.on('click', function () {
             var value = parseFloat(obj.val());
 
             value += settings.step;
@@ -65,23 +65,26 @@
         return button;
     };
 
-    $.fn.spinner = function(s) {
+    $.fn.spinner = function (s) {
         var settings = $.extend({}, $.spinner.defaults, s);
-        this.addClass('spinner__count');
-        this.before(downButton(this, settings));
-        this.after(upButton(this, settings));
-        this.on('keyup', function() {
-            var el = $(this),
-                value = el.val();
 
-            if (settings.max != null && value > settings.max) {
-                el.val(settings.max);
-            }
+        return this.each(function () {
+            var $this = $(this);
+            $this.addClass('spinner__count')
+                .before(downButton($this, settings))
+                .after(upButton($this, settings))
+                .on('keyup', function () {
+                    var el = $(this),
+                        value = el.val();
 
-            if (settings.min != null && value < settings.min) {
-                el.val(settings.min);
-            }
+                    if (settings.max != null && value > settings.max) {
+                        el.val(settings.max);
+                    }
+
+                    if (settings.min != null && value < settings.min) {
+                        el.val(settings.min);
+                    }
+                });
         });
-        return this;
     };
 })(jQuery);
